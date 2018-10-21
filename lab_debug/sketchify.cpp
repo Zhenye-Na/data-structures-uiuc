aedae5c0520366dcf5ebb5fa3e1152178cf7169a
Zhenye Na <zna2@illinois.edu>
2018-10-20 20:06:31 -0500

    Finished lab_debug :ok_hand:

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -7,54 +7,56 @@ using namespace cs225;
 
 // sets up the output image
 PNG* setupOutput(unsigned w, unsigned h) {
-    PNG* image = new PNG(w, h);
-    return image;
+  PNG* image = new PNG(w, h);
+  return image;
 }
 
 // Returns my favorite color
 HSLAPixel* myFavoriteColor(double saturation) {
-    HSLAPixel pixel(-1, saturation, 0.5);
-    return &pixel;
+  // HSLAPixel pixel(278, saturation, 0.5);
+  return new HSLAPixel(278, saturation, 0.5);
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
-    // Load in.png
-    PNG* original = NULL;
+  // Load in.png
+  PNG* original = new PNG();
 
-    original->readFromFile(inputFile);
-    unsigned width = original->width();
-    unsigned height = original->height();
+  original->readFromFile(inputFile);
+  unsigned width = original->width();
+  unsigned height = original->height();
 
-    // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+  // Create out.png
+  PNG* output = nullptr;
+  output = setupOutput(width, height);
 
-    // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor(0.5);
+  // Load our favorite color to color the outline
+  HSLAPixel* myPixel = myFavoriteColor(0.5);
 
-    // Go over the whole image, and if a pixel differs from that to its upper
-    // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
-            // Calculate the pixel difference
-            HSLAPixel & prev = original->getPixel(x - 1, y - 1);
-            HSLAPixel & curr = original->getPixel(x, y);
-            double diff = std::fabs(curr.h - prev.h);
+  // Go over the whole image, and if a pixel differs from that to its upper
+  // left, color it my favorite color in the output
+  for (unsigned y = 1; 0 < y && y < height; y++) {
+    for (unsigned x = 1; 0 < x && x < width; x++) {
+      // Calculate the pixel difference
+      HSLAPixel & prev = original->getPixel(x - 1, y - 1);
+      HSLAPixel & curr = original->getPixel(x, y);
+      double diff = std::fabs(curr.h - prev.h);
 
-            // If the pixel is an edge pixel,
-            // color the output pixel with my favorite color
-            HSLAPixel * currOutPixel = &(output->getPixel(x, y));
-            if (diff > 20) {
-                currOutPixel = myPixel;
-            }
-        }
+      // If the pixel is an edge pixel,
+      // color the output pixel with my favorite color
+      HSLAPixel* currOutPixel = &(output->getPixel(x, y));
+      if (diff > 20) {
+        // cout << *myPixel << endl;
+        // cout << *currOutPixel << endl;
+        *currOutPixel = *myPixel;
+      }
     }
+  }
 
-    // Save the output file
-    output->writeToFile(outputFile);
+  // Save the output file
+  output->writeToFile(outputFile);
 
-    // Clean up memory
-    delete myPixel;
-    delete output;
-    delete original;
+  // Clean up memory
+  delete myPixel;
+  delete output;
+  delete original;
 }
