4435de03e37705529bf424e64e0f5197ce36d6e2
Zhenye Na <zna2@illinois.edu>
2018-11-12 15:38:53 -0600

    add StickerSheet

---
+++ mp2/StickerSheet.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file StickerSheet.h
+ * Contains your implementation of the interface for the StickerSheet class.
+ */
+
+#include "StickerSheet.h"
+#include "cs225/PNG.h"
+#include "cs225/HSLAPixel.h"
--- mp2/StickerSheet.h
+++ mp2/StickerSheet.h
@@ -2,9 +2,36 @@
  * @file StickerSheet.h
  * Contains your declaration of the interface for the StickerSheet class.
  */
+
+ #include "Image.h"
+ #include <iostream>
+
 #ifndef STICKERSHEET_H_
 #define STICKERSHEET_H_
 
+namespace cs225 {
+  class StickerSheet: public Image {
+  public:
+    StickerSheet(const Image & picture, unsigned max);
+    ~StickerSheet();
+    StickerSheet(const StickerSheet & other);
+
+
+    const StickerSheet& StickerSheet::operator=	(const StickerSheet & other);
+
+    void StickerSheet::changeMaxStickers(unsigned max);
+
+    int StickerSheet::addSticker(Image & sticker, unsigned x, unsigned y);
+
+    bool StickerSheet::translate(unsigned index, unsigned x, unsigned y;
+
+    void StickerSheet::removeSticker(unsigned index);
+
+    Image* StickerSheet::getSticker(unsigned index) const;
+
+    Image StickerSheet::render() const;
+
+  }
+};
 
 #endif
- 
