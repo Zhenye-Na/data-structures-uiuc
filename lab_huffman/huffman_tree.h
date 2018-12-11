6ed5f646cdcc7a6b2b463bedd579384c0f4e40c4
Zhenye Na <zna2@illinois.edu>
2018-12-10 23:45:46 -0600

    lab_huffman initial commit

--- lab_huffman/huffman_tree.h
+++ lab_huffman/huffman_tree.h
@@ -266,11 +266,11 @@ class HuffmanTree
     /**
      * Root of the HuffmanTree.
      */
-    TreeNode* root_;                            
+    TreeNode* root_;
 
     /**
      * Standard map that maps characters to their encoded values.
      */
-    std::map<char, std::vector<bool>> bitsMap_; 
+    std::map<char, std::vector<bool>> bitsMap_;
 };
 #endif
